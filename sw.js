c2acaea99d34034be0669e4943b0fa98f77bb537
OUOzOUO <165500772+OUOzOUO@users.noreply.github.com>
2026-05-07 08:36:12 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'chronos-v6-cache';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './192x192.png',
+  './512x512.png'
+];
+
+// 安裝時：快取所有核心檔案
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 攔截網路請求：優先使用快取資料
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
